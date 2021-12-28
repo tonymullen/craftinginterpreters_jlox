8c8c684ecb84473631824bf8ef88158d5efbedce
Tony Mullen <tony.mullen@gmail.com>
2021-12-27 23:36:21 -0800

    Adds GenerateAST tool

---
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreters.lox;
+
+abstract class Expr {
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+}
