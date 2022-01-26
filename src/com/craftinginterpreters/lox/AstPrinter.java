d7191b16ef7ae5fd54cdfe0b27e425e3a4d21b5b
Tony Mullen <tony.mullen@gmail.com>
2022-01-26 12:27:34 -0800

    Updates GenerateAst.java

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -32,6 +32,11 @@ class AstPrinter implements Expr.Visitor<String> {
         return null;
     }
 
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return null;
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
