4812046ba5e600578e99c9e5bf176123c9d5cbc3
Tony Mullen <tony.mullen@gmail.com>
2022-02-01 17:51:41 -0800

    Implement for loop

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -154,6 +154,14 @@ public class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+        return null;
+    }
+
+    @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
         environment.assign(expr.name, value);
